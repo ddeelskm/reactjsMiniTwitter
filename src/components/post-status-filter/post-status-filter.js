c15c71c830c8e4d918b4dc581fe2566902c38fa1
ddeelskm <ddeelskm@gmail.com>
2020-07-01 14:51:12 +0400

    reactjsMiniTwitter

---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './post-status-filter.css'
+
+const PostStatusFilter = () => {
+    return (
+        <div className='btn-group'>
+            <button/>
+            <button/>
+        </div>
+    )
+}
+export default PostStatusFilter
